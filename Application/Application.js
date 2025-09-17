f5b1817578ebbb8633f10fee3f8774900fd15895
Sara Statoua <sara.statoua@gmail.com>
2025-09-17 11:37:53 +0200

    d

--- Application/Application.js
+++ Application/Application.js
@@ -1,16 +1,30 @@
-let die = []
+let die = [];
 
-for(let index = 1; index <= 5; index++){
-    const dice = {eyes: index,
-        roll: function(){
-            this.eyes = Math.trunc(Math.random() * 6) + 1
-        }
-    }
-    die.push(dice)
+for (let index = 1; index <= 5; index++) {
+  const dice = {
+    eyes: index,
+    hold: false,
+    roll: function () {
+      if (!this.hold) {
+        this.eyes = Math.trunc(Math.random() * 6) + 1;
+      }
+    },
+  };
+  die.push(dice);
 }
 
 for (const element of die) {
-    element.roll()
-    console.log(element.eyes);
+  element.roll();
+  console.log(element.eyes);
 }
-//joe
+
+function upperSectionScore(eyes) {
+  sum = 0;
+  for (const element of die) {
+    if (element.eyes === eyes) {
+      sum += eyes;
+    }
+  }
+  return sum;
+}
+console.log("antal 1'er: ",upperSectionScore(1));
